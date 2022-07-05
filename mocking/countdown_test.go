db516f50425373ae9fa9182fe4b620d4f12d4811
Haowei Lee <haoweilee.tpe@gmail.com>
2022-07-05 20:46:54 +0200

    mid mocking

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -1,1 +1,35 @@
-package mocking
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountDown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+
+	Countdown(buffer, spySleeper)
+
+	got := buffer.String()
+	want := `3
+2
+1
+Go!`
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to slpper, want 3 got %d", spySleeper.Calls)
+	}
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
